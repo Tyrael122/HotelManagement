4bb40f591e47f82f5cdde732afbe02a92244f27d
Tyrael122 <kanbme@protonmail.com>
2023-02-17 22:47:10 -0300

    MySqlDump

--- sqlJob.sql
+++
@@ -1,9 +0,0 @@
-delimiter |
-CREATE EVENT roomStatusUpdater
-    ON SCHEDULE EVERY 1 MINUTE
-    DO
-		BEGIN
-			UPDATE room INNER JOIN statustimelinelog ON room.Id = statustimelinelog.RoomId SET room.Status = statustimelinelog.Status
-			WHERE statustimelinelog.Date <= now();
-		END |
-delimiter ;
